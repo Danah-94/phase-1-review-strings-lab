25a234b4a655fd3e4c8d33531e33d5dfa206741b
Danah <osta.danah@gmail.com>
2022-10-12 18:02:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+
+const currentUser = 'Mia';
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+let shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+
+
+
+    
+
+
